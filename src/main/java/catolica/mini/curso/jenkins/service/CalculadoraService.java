d023218428faf9dae0e87f3cdd5b50e903156662
Laires <lairespsoares@gmail.com>
2023-12-06 21:23:04 -0300

    first jenkins commit

---
+++ src/main/java/catolica/mini/curso/jenkins/service/CalculadoraService.java
@@ -0,0 +1,16 @@
+package catolica.mini.curso.jenkins.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculadoraService {
+    public String hello() {
+        return  "Bem-vindo ao curos de Jenkins";
+    }
+    public int somar(int a, int b) {
+        return a + b;
+    }
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+}
